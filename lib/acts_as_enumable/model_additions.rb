3c69ac820ce562f99d09e9cd89370d5ed0d02cff
Tomasz Łoszko <tomasz.loszko@gmail.com>
2012-02-27 00:12:07 +0100

    changed gem behaviour to the one of enum_fu

--- lib/acts_as_enumable/model_additions.rb
+++ lib/acts_as_enumable/model_additions.rb
@@ -1,109 +1,42 @@
 module ActsAsEnumable
   module ModelAdditions
-    # To make the attribute an enum.
-    #
-    #   class User < ActiveRecord::Base
-    #     # assumes the column exists:
-    #     #   integer :role_enum
-    #
-    #     acts_as_enumable(:role, %w(admin staff helper member), defaul: "member")
-    #   end
-    #
-    # Available options are:
-    #
-    #   (optional) default: String / Symbol
-    #     default value, will be set to nil if it is not provided or it does not match the preset list
-    #     eg.
-    #     acts_as_enumable :role, %w(admin staff), default: :something_not_exist # set the default value to nil
-    #     acts_as_enumable :role, %w(admin staff), default: "staff" # set the defaut value to "staff"
-    #
-    # This enables the following methods:
-    #
-    #   user = User.first
-    #   User.roles
-    #   # ["admin", "staff", "helper", "member"]
-    #
-    #   User.roles_for_select("users.roles")
-    #   # [
-    #   #   { key: "admin", value: I18n.t("users.roles.admin") },
-    #   #   { key: "staff", value: I18n.t("users.roles.staff") }, ...
-    #   # ]
-    #
-    #   User.default_role
-    #   # "member"
-    #
-    #   User.default_role_enum
-    #   # 3
-    #
-    #   user.role
-    #   # "member"
-    #
-    #   user.role = "staff" # or user.role = :staff
-    #   user.role
-    #   # "staff"
-    #
-    #   user.role_enum # if user.role == "staff"
-    #   # 1
-    #
-    #   user.role_enum = 0
-    #   user.role
-    #   # "admin"
-    def acts_as_enumable(attribute_name, enum_values, options={})
-      attribute = attribute_name.to_s
-      options[:default] ||= nil
+    def acts_as_enumable attribute, enum_values, options={}
+      attribute = attribute.to_s
+      attr_plural = attribute.pluralize
+
+      enum_values.collect!(&:to_sym)
 
       class_eval %Q{
-        def self.#{attribute.pluralize}
+        def self.#{attr_plural}
           #{enum_values}
         end
       }
 
       class_eval %Q{
-        def self.#{attribute.pluralize}_for_select(i18n_namespace)
-          #{attribute.pluralize}.map do |value|
+        def self.#{attr_plural}_for_select(i18n_namespace)
+          #{attr_plural}.map do |value|
             { key: value, value: I18n.t("\#{i18n_namespace}.\#{value}") }
           end
         end
       }
 
       class_eval %Q{
-        def self.default_#{attribute}
-          #{options[:default].nil? ? "nil" : "\"#{options[:default]}\""}
+        def self.#{attribute} symbol
+          #{enum_values}.index(symbol.to_sym)
         end
-      }
+       }
 
-      class_eval %Q{
-        def self.default_#{attribute}_enum
-          #{attribute.pluralize}.index(default_#{attribute})
-        end
-      }
-
-      define_method "value_of_#{attribute}" do |symbol|
-        self.class.send(attribute.pluralize).index(symbol.to_s)
+      define_method attribute do
+        index = read_attribute(attribute)
+        return nil if index.nil?
+        enum_values.at(index)
       end
 
-      define_method "symbol_of_#{attribute}" do |value|
-        self.class.send(attribute.pluralize)[value]
+      define_method "#{attribute}=" do |symbol|
+        value = enum_values.index(symbol)
+        write_attribute(attribute, value)
       end
 
-      define_method attribute_name do
-        value = send("#{attribute_name}_enum")
-        return nil if value.nil?
-        self.class.send(attribute.pluralize)[value]
-      end
-      define_method "#{attribute_name}=" do |symbol|
-        value = self.send("value_of_#{attribute}", symbol)
-        send("#{attribute_name}_enum=", value)
-      end
-
-      define_method "#{attribute_name}_enum" do
-        read_attribute(:"#{attribute_name}_enum") || self.class.send("default_#{attribute_name}_enum")
-      end
-      define_method "#{attribute_name}_enum=" do |value_index|
-        counts = self.class.send(attribute.pluralize).count
-        value_index = (0...counts).include?(value_index) ? value_index : self.class.send("default_#{attribute_name}_enum")
-        write_attribute(:"#{attribute_name}_enum", value_index)
-      end
     end
   end
 end
--- spec/acts_as_enumable/model_additions_spec.rb
+++ spec/acts_as_enumable/model_additions_spec.rb
@@ -2,105 +2,73 @@
 require "spec_helper"
 
 class User < SuperModel::Base
-  def role_enum
-    read_attribute(:role_enum)
+  def role
+    read_attribute(:role)
   end
-  def role_enum=(val)
-    write_attribute(:role_enum, val)
+  def role=(val)
+    write_attribute(:role, val)
   end
   
   extend ActsAsEnumable::ModelAdditions
   acts_as_enumable :role, %w(admin staff helper member)
-  acts_as_enumable :status, %w(active inactive), default: "inactive"
+  acts_as_enumable :status, %w(active inactive)
 end
 
 describe ActsAsEnumable::ModelAdditions do
   it "creates a constant given the attribute name and values" do
-    User.roles.should == %w(admin staff helper member)
-    User.statuses.should == %w(active inactive)
+    User.roles.should == [:admin, :staff, :helper, :member]
+    User.statuses.should == [:active, :inactive]
   end
 
-  it "is a testing function..." do
-    User.roles.should == %w(admin staff helper member)
+  it "returns index of given role" do
+      [:admin, :staff, :helper, :member].each_with_index do |role,i|
+        User.role(role).should == i
+      end
   end
+
 
   it "returns an array to be used for select" do
     User.roles_for_select("test.namespace").should == [{
-      key: "admin", value: I18n.t("test.namespace.admin")
+      key: :admin, value: I18n.t("test.namespace.admin")
     }, {
-      key: "staff", value: I18n.t("test.namespace.staff")
+      key: :staff, value: I18n.t("test.namespace.staff")
     }, {
-      key: "helper", value: I18n.t("test.namespace.helper")
+      key: :helper, value: I18n.t("test.namespace.helper")
     }, {
-      key: "member", value: I18n.t("test.namespace.member")
+      key: :member, value: I18n.t("test.namespace.member")
     }]
 
     User.statuses_for_select("test.namespace").should == [{
-      key: "active", value: I18n.t("test.namespace.active")
+      key: :active, value: I18n.t("test.namespace.active")
     }, {
-      key: "inactive", value: I18n.t("test.namespace.inactive")
+      key: :inactive, value: I18n.t("test.namespace.inactive")
     }]
   end
 
-  it "returns the default value" do
-    User.default_role.should be_nil
-    User.default_status.should == "inactive"
-  end
 
-  it "returns the default value index" do
-    User.default_role_enum.should be_nil
-    User.default_status_enum.should == 1
-  end
-
-  it "handles value correctly" do
+  it "assigns correct value" do
     user = User.new
-    user.role_enum.should be_nil
     user.role.should be_nil
-    user.status_enum.should == 1
-    user.status.should == "inactive"
+    user.status.should be_nil
 
-    user.role_enum = 1
-    user.role_enum.should == 1
-    user.role.should == "staff"
-    user.status_enum = 0
-    user.status_enum.should == 0
-    user.status.should == "active"
+     [:admin, :staff, :helper, :member].each_with_index do |role,i|
 
-    user.role = "admin"
-    user.role_enum.should == 0
-    user.role.should == "admin"
-    user.status = "inactive"
-    user.status_enum.should == 1
-    user.status.should == "inactive"
+      user.role = role
+      user.send(:read_attribute, :role).should == i
+      user.role.should == role
+    
+    end
 
-    # error cases
+    user.status = :inactive
+    user.send(:read_attribute, :status).should == 1
+    user.status.should == :inactive
+  end
 
+  it "handles incorrect enum values" do
+    user = User.new
     user.role = "not existing role"
-    user.role_enum.should be_nil # as nil is the default value for role
     user.role.should be_nil
-    user.status = "not existing status"
-    user.status_enum.should == 1 # as 1 (i.e. inactive) is the default value for status
-    user.status.should == "inactive"
-
-    user.role_enum = 1234
-    user.role_enum.should be_nil # as nil is the default value for role
-    user.role.should be_nil
-    user.status_enum = 1234
-    user.status_enum.should == 1
-    user.status.should == "inactive"
+    user.send(:read_attribute, :role).should be_nil
   end
 
-  it "convert value to symbol" do
-    user = User.new
-
-    user.value_of_role(:admin).should == 0
-    user.value_of_status(:inactive).should == 1
-  end
-
-  it "convert symbol to value" do
-    user = User.new
-
-    user.symbol_of_role(1).should == "staff"
-    user.symbol_of_status(0).should == "active"
-  end
 end
